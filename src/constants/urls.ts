d48b67deb57aedde4edb0cc272e9834f7b3d99d1
Guillermo Menguez Alvarez <guillermo.menguez.alvarez@hp.com>
2025-03-20 12:28:26 +0100

    Update backend url

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,2 +1,2 @@
-export const SALAMANCA_MONUMENTS_URL = 'https://monuments.milicia.net/monuments/localidad/Salamanca';
-export const IMAGE_URL_GETTER = (id: number): string => `https://monuments.milicia.net/monuments/${id}/image`;
+export const SALAMANCA_MONUMENTS_URL = 'http://nonia6.duckdns.org:7000/monuments/localidad/Salamanca';
+export const IMAGE_URL_GETTER = (id: number): string => `http://nonia6.duckdns.org:7000/monuments/${id}/image`;
